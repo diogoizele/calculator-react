777b97ec79d1f18f3149c631a227c9a515d91b47
Diogo Izele <diogogabrielizele@gmail.com>
2021-08-30 21:18:59 -0300

    implemented button functions receiving their content

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,22 @@
 import React from "react";
-import './Button.css'
+import "./Button.css";
 
 const Button = (props) => {
-  return <button className="button">{props.label}</button>;
+  const applyClasses = () => {
+    let classes = "button ";
+    classes += props.operation ? "operation" : "";
+    classes += props.double ? "double" : "";
+    return classes;
+  };
+
+  return (
+    <button
+      onClick={(e) => props.click && props.click(props.label)}
+      className={applyClasses()}
+    >
+      {props.label}
+    </button>
+  );
 };
 
 export default Button;
--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -5,28 +5,38 @@ import Button from "./Button.jsx";
 import Display from "./Display.jsx";
 
 const Calculator = (props) => {
+  const clearMemory = () => {
+    console.log("limpar");
+  };
+
+  const setOperation = (operation) => {
+    console.log(operation);
+  };
+  const addDigit = (digit) => {
+    console.log(digit);
+  };
   return (
     <div className="calculator">
       <Display value="100" />
-      <Button label="AC" />
+      <Button label="AC" click={clearMemory} />
       <Button label="+/-" />
       <Button label="%" />
-      <Button label="÷" />
-      <Button label="7" />
-      <Button label="8" />
-      <Button label="9" />
-      <Button label="×" />
-      <Button label="4" />
-      <Button label="5" />
-      <Button label="6" />
-      <Button label="-" />
-      <Button label="1" />
-      <Button label="2" />
-      <Button label="3" />
-      <Button label="+" />
-      <Button label="0" />
+      <Button operation label="÷" click={() => setOperation("/")} />
+      <Button label="7" click={addDigit} />
+      <Button label="8" click={addDigit} />
+      <Button label="9" click={addDigit} />
+      <Button operation label="×" click={() => setOperation("*")} />
+      <Button label="4" click={addDigit} />
+      <Button label="5" click={addDigit} />
+      <Button label="6" click={addDigit} />
+      <Button operation label="-" click={setOperation} />
+      <Button label="1" click={addDigit} />
+      <Button label="2" click={addDigit} />
+      <Button label="3" click={addDigit} />
+      <Button operation label="+" click={setOperation} />
+      <Button double label="0" click={addDigit} />
       <Button label="," />
-      <Button label="=" />
+      <Button operation label="=" click={setOperation} />
     </div>
   );
 };
